e46076913af00a853d3e68e67e661181d11e62e0
Takuji Nishibayashi <takuji31@gmail.com>
2021-01-03 16:02:04 +0900

    Update Deps for library

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -52,13 +52,11 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.3.2")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("com.google.android.material:material:1.2.1")
     implementation("androidx.compose.ui:ui:${Libs.composeVersion}")
     implementation("androidx.compose.material:material:${Libs.composeVersion}")
     implementation("androidx.compose.ui:ui-tooling:${Libs.composeVersion}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.0-rc01")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.0-rc01")
     testImplementation("junit:junit:4.13.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
